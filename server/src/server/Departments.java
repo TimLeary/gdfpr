fc3f752a4a1de4d2b7f4fd5138b667d4b3edca5d
dnaszta <david.nasztanovics@stylersonline.com>
2017-06-21 01:26:09 +0200

    remove print

--- server/src/server/Departments.java
+++ server/src/server/Departments.java
@@ -27,7 +27,6 @@ public class Departments extends UnicastRemoteObject implements DepartmentsInter
         String name = result.getString("depName");
         int managerId = result.getInt("manId");
         Department dept= new Department(id, name, managerId);
-        System.out.println(dept);
         list.add(dept);
       }
 
